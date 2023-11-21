37aff01409b479a9620419e232c16301dfdd8996
MelekAzizova <147470006+MelekAzizova@users.noreply.github.com>
2023-11-21 23:43:03 +0400

    SQL Task1

--- SQLTask2.sql
+++ SQLTask2.sql
@@ -1,6 +1,5 @@
-﻿--CREATE DATABASE AzMB101_MelekAzizova
---USE AzMB101_MelekAzizova
-
+﻿--CREATE DATABASE AzMB101
+--USE AzMB101
 CREATE TABLE Position
 (
 	Id int identity primary key,
@@ -66,17 +65,19 @@ CREATE TABLE Sale
 	Id int identity primary key,
 	SaleDate date,
 	ProductId int REFERENCES Product(Id),
-	EmployeeId int REFERENCES Employee(Id)
+	EmployeeId int REFERENCES Employee(Id),
+	BranchId int REFERENCES Branch(Id)
 )
 
 
-INSERT INTO Sale VALUES ('2022-12-15', 1, 3);
-INSERT INTO Sale VALUES ('2023-11-15', 5, 3);
-INSERT INTO Sale VALUES ('2023-09-16', 4, 3);
-INSERT INTO Sale VALUES ('2022-07-23', 3, 4);
-INSERT INTO Sale VALUES ('2023-11-28', 3, 7);
-INSERT INTO Sale VALUES ('2023-09-29', 1, 6);
-INSERT INTO Sale VALUES ('2023-06-18', 4, 4);
+INSERT INTO Sale VALUES ('2022-12-15', 1, 3,5);
+INSERT INTO Sale VALUES ('2023-11-15', 4, 3,2);
+INSERT INTO Sale VALUES ('2023-09-16', 4, 3,1);
+INSERT INTO Sale VALUES ('2022-07-23', 3, 4,2);
+INSERT INTO Sale VALUES ('2023-11-28', 3, 7,4);
+INSERT INTO Sale VALUES ('2023-09-29', 1, 6,3);
+INSERT INTO Sale VALUES ('2023-06-18', 4, 4,5);
+INSERT INTO Sale VALUES ('2023-11-21', 4, 3,5);
 
 SELECT*FROM Employee 
 SELECT*FROM Product
@@ -91,11 +92,13 @@ SELECT*FROM Position
 
 SELECT
     Sale.Id,
-    Sale.SaleDate,
     Employee.Name,
     Product.Name,
+	Branch.Name,
     Product.PurchasePrice,
-    Product.SalePrice
+    Product.SalePrice,
+    Sale.SaleDate
+	
 	
 FROM
     Sale
@@ -103,17 +106,57 @@ JOIN
     Employee ON Sale.EmployeeId = Employee.Id
 JOIN
     Product ON Sale.ProductId = Product.Id
+JOIN
+    Branch ON Sale.BranchId = Branch.Id
 
 ---------------------------------------------
-SELECT SUM(SalePrice) AS SumPrice
-FROM Product
+SELECT SUM(Product.SalePrice) AS SumPrice
+FROM Sale
+JOIN Product ON Sale.ProductId=Product.Id
 
 ----------------------------------------------
---errorlu kod
---SELECT SUM(SalePrice) AS SumPrice
---FROM Sale
---JOIN Product ON  Sale.ProductId=Product.Id
---SELECT EXTRACT(MONTH FROM  '2022-12-15');
+CREATE VIEW vw_CountProductsSold AS
+(SELECT e.Id,e.Name,e.Surname,COUNT(p.Id) AS ProductCOUNT  FROM Employee AS e
+JOIN Sale AS s
+ON s.EmployeeId=e.Id
+JOIN Product AS p
+ON p.Id=s.ProductId
+GROUP BY e.Id,e.Name,e.Surname)
+
+SELECT * FROM vw_CountProductsSold
+
+---------------------------------------------ELE BELE
+--SELECT MAX(ProductCOUNT) AS Products FROM
+--(SELECT e.Id,e.Name,e.Surname,COUNT(p.Id) AS ProductCOUNT  FROM Employee AS e
+--JOIN Sale AS s
+--ON s.EmployeeId=e.Id
+--JOIN Product AS p
+--ON p.Id=s.ProductId
+--GROUP BY e.Id,e.Name,e.Surname) AS Table1 
+
+------------------------------------------------------------
+
+CREATE VIEW vw_BranchSell AS
+(SELECT b.Id,b.Name ,SUM(p.SalePrice) AS TotalPrice
+FROM Sale AS s
+JOIN  Branch AS b
+ON s.BranchId=b.Id
+JOIN Product AS p
+ON s.ProductId=p.Id
+WHERE S.SaleDate=CONVERT(date,GETDATE())
+GROUP BY b.Id,b.Name
+)
+
+SELECT*FROM vw_BranchSell
+
+
+
+--WHERE
+--    Sale.SaleDate = CONVERT(date, GETDATE())  -- Filter by the current date
+--GROUP BY
+--    Branch.Id, Branch.Name
+--ORDER BY
+--    TotalSales DESC
 
 
 
